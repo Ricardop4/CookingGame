c9aedd9a7bce72e51a3bc998b5541cb3f50862d9
Ricardop4 <Ricardop4@learn.cssd.ab.ca>
2022-03-29 13:03:09 -0600

    Deleted some unwanted sprites and Character can now move

---
+++ Cooking Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] Rigidbody2D rb;
+    Vector2 movement;
+
+    // cached references
+    Rigidbody2D myRigidBody2D;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        //Movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
