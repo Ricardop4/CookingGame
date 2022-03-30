060c669747f8d78ec89336f608b67ecffdecec2f
Ricardop4 <Ricardop4@learn.cssd.ab.ca>
2022-03-30 13:19:29 -0600

    Made many typos and fixed them

---
+++ Cooking Game/Assets/Scripts/Inventory.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] public bool[] isFull;
+    [SerializeField] public GameObject[] slots;
+}
---
+++ Cooking Game/Assets/Scripts/PickupItem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupItem : MonoBehaviour
+{
+    private Inventory inventory;
+    [SerializeField] GameObject itemButton;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    public async void OnTriggerEnter2D(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            for(int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    //Item can be added
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
