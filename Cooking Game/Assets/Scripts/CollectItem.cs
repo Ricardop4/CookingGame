6a538c4006c0ddeb040abb8bcb427063c218bd9d
Ricardop4 <Ricardop4@learn.cssd.ab.ca>
2022-03-30 11:33:13 -0600

    Items can now be collected yay

---
+++ Cooking Game/Assets/Scripts/CollectItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectItem : MonoBehaviour
+{   
+    [SerializeField] string itemName;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log($"{itemName} has been collected");
+        Destroy(gameObject);
+    }
+}
