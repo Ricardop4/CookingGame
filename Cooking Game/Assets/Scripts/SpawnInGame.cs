d095e2b6892e3c36fc49a5abfc705e84bb63a12c
Ricardop4 <Ricardop4@learn.cssd.ab.ca>
2022-04-04 14:28:09 -0600

    Saved for April 4

---
+++ Cooking Game/Assets/Scripts/SpawnInGame.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnInGame : MonoBehaviour
+{
+    [SerializeField] int items;
+    [SerializeField] GameObject item;    
+    [SerializeField] float minX;
+    [SerializeField] float maxX;
+    [SerializeField] float minY;
+    [SerializeField] float maxY;
+
+
+    void Start()
+    {
+        while(true)
+        {
+        StartCoroutine(ExampleCoroutine());
+        }
+    }
+    IEnumerator ExampleCoroutine()
+    {
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(2);
+        SpawnItem();
+    }
+
+    private void SpawnItem()
+    {
+        Vector2 newPos = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+        Instantiate(item, newPos, Quaternion.identity);
+    }
+}
