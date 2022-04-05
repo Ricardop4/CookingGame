6bf56c5067907773dd491ad58cbfe181e55842ae
Ricardop4 <Ricardop4@learn.cssd.ab.ca>
2022-04-05 14:29:55 -0600

    Saved for April 5

---
+++ Cooking Game/Assets/Scripts/Score.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] int foodPoints = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+
+    // state variables
+    [SerializeField] int currentScore = 0;
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
+    public void AddToScore()
+    {
+        currentScore += foodPoints;
+        scoreText.text = currentScore.ToString();
+    }
+
+}
